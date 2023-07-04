5d105ee3f7914b1e8e2d10942dffb5599ff1705b
shreshtha-132 <shreshthakumargupta@gmail.com>
2023-07-04 13:14:12 +0530

    project done
    -> font size
    -> font color
    -> font family and font style
    -> icons
    -> padding
    -> margin
    -> sized box
    -> card
    -> list tile
    -> divider
      next gotta add the calling and mailing functionality to the list tiles.

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,52 @@ class MyApp extends StatelessWidget {
                 backgroundColor: Colors.greenAccent,
                 radius: 120,
               ),
-              Text("Shreshtha"),
-              Text("Flutter Developer"),
+              Text(
+                "Shreshtha",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                    fontSize: 25, color: Colors.white, letterSpacing: 1.5),
+              ),
+              SizedBox(
+                width: 350,
+                height: 75,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  // padding: EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 25,
+                    ),
+                    title: Text(
+                      "8381931351",
+                      style: TextStyle(fontSize: 25, color: Colors.teal),
+                    ),
+                  )),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  // padding: EdgeInsets.all(3),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(Icons.mail, color: Colors.teal),
+                    title: Text(
+                      "shreshthakumargupta@gmail.com",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
+                    ),
+                  )),
             ],
           ),
         ),
@@ -33,3 +77,37 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Row(
+// children: [
+// Icon(
+// Icons.email,
+// color: Colors.teal,
+// size: 39,
+// ),
+// SizedBox(
+// width: 10,
+// ),
+// Text(
+// "shreshthakumargupta@gmail.com",
+// style: TextStyle(color: Colors.teal, fontSize: 25),
+
+// Row(
+// children: [
+// Icon(
+// Icons.phone,
+// size: 39,
+// color: Colors.teal,
+// ),
+// SizedBox(
+// width: 10,
+// ),
+// Text(
+// "8381931351",
+// style: TextStyle(fontSize: 30, color: Colors.teal),
+// ),
+// ],
+// ),
+// ),
+// ],
+// ),
